f855a3d37e41e323d181068146434a2d12c49283
dedb0r0ded <revikad.00@gmail.com>
2026-03-26 10:16:40 +0800

    Created common, array and list directories. Started common code part development.

---
+++ src/common/common.h
@@ -0,0 +1,26 @@
+// cОmmon.h
+// Pragmas
+#pragma once
+
+
+// Defines
+// Conditional compilation flags
+#define ACCOUNTING_THREAD_SAFE_TIME
+#define USE_CUSTOM_DATE_TIME
+
+
+// C++
+#include <mutex>
+#include <type_traits>
+
+// Libs
+#include "nlohmann/json.hpp"
+#include "./math.h"             // <cmath>
+#include "./enums.h"            // "./aliases.h" <- {<optional>, <memory>, "./strings.h"}
+#include "./time.h"             // {<ctime>, <string>, <sstream>, <iomanip>, "./aliases.h" <- {<cstdint>, <optional>, <memory>, <chrono>}
+
+#ifdef USE_CUSTOM_DATE_TIME
+#include "./date_time.h"
+#endif
+
+using namespace std::string_literals;
